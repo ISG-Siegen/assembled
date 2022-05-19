a81dadbc57f115e8cbe7a40d238d92159cc7d8c7
LennartPurucker <lennart.purucker@uni-siegen.de>
2022-05-19 12:50:53 +0200

    add openml test not in git yet

---
+++ test/test_assembledopenml/test_openml_assembler.py
@@ -0,0 +1,27 @@
+from assembledopenml.openml_assembler import OpenMLAssembler
+import pytest
+
+
+class TestOpenMLAssembler:
+
+    def test_assembler_init(self):
+        with pytest.raises(ValueError):
+            OpenMLAssembler("möp")
+
+    def test_assembler_run(self):
+        omla = OpenMLAssembler(nr_base_models=5)
+
+        mt = omla.run(3)
+
+        assert mt.dataset is not None
+        assert mt.dataset_name is not None
+        assert mt.target_name is not None
+        assert mt.class_labels is not None
+        assert mt.cat_feature_names is not None
+        assert mt.task_type is not None
+        assert mt.is_classification is not None
+        assert mt.is_regression is not None
+        assert mt.openml_task_id is not None
+        assert mt.folds_indicator is not None
+
+        assert len(mt.predictors) == 5
