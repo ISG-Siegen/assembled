f7f4c5a8e3d9df5b5ded83846e35a26a193be478
LennartPurucker <lennart.purucker@uni-siegen.de>
2022-05-20 14:40:51 +0200

    updated old benchmark details and rebuild to fit to new structure

--- examples/example_rebuild_benchmark_data.py
+++ examples/example_rebuild_benchmark_data.py
@@ -6,8 +6,10 @@ The metatasks that this script creates, contain the results of post-processing a
 the tag "OpenML-CC18".
 
 We will store these benchmark metatasks in their own directory.
-We re-crawl the associated metatasks such that a user would not have to crawl the related pre-post-processed
+We re-get the associated metatasks such that a user would not have to crawl the related pre-post-processed
 metatasks first.
+
+Please be aware, this requires a benchmark to be build from OpenML metatasks!
 """
 
 import os
@@ -22,16 +24,14 @@ with open(file_path_json) as json_file:
     benchmark_meta_data = json.load(json_file)
 valid_task_ids = benchmark_meta_data["valid_task_ids"]
 task_ids_to_valid_predictors = benchmark_meta_data["task_ids_to_valid_predictors"]
-selection_constraints = benchmark_meta_data["selection_constraints"]
-
-# -- Crawl Original Metatask and remove non-valid predictors
-omla = OpenMLAssembler(openml_metric_name=selection_constraints["openml_metric_name"],
-                       maximize_metric=selection_constraints["maximize_metric"],
-                       nr_base_models=selection_constraints["nr_base_models"])
+selection_constraints_per_task = benchmark_meta_data["selection_constraints_per_task"]
 
 nr_tasks = len(valid_task_ids)
 for task_nr, task_id in enumerate(valid_task_ids, start=1):
     print("#### Process Task {} ({}/{}) ####".format(task_id, task_nr, nr_tasks))
+    omla = OpenMLAssembler(openml_metric_name=selection_constraints_per_task[task_id]["openml_metric_name"],
+                           maximize_metric=selection_constraints_per_task[task_id]["maximize_metric"],
+                           nr_base_models=selection_constraints_per_task[task_id]["nr_base_models"])
 
     # Crawl Metatask
     valid_predictors = task_ids_to_valid_predictors[str(task_id)]
